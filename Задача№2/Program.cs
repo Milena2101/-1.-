e7d3da0db0e6f95bf9052b5839f8c0f9891fd68d
Milena2101 <filimonova.milena2015@yandex.ru>
2023-02-11 21:59:08 +0300

    №2Решение

---
+++ Задача№2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = -13, b = -6;
+if (a > b)
+{
+    Console.WriteLine("Max number = " + a);
+    Console.WriteLine("Min nunber = " + b);
+}
+else 
+{
+    Console.WriteLine("Max number = " + b);
+    Console.WriteLine("Min number = " + a);
+}
+Console.ReadLine();
